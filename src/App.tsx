ee1c545b5c86e9e1f3e8ac63f5fec5090c9111df
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-03-05 16:30:54 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{FC, useContext, useEffect, useState}from 'react';
+import { Context } from '.';
+import LoginForm from './components/LoginForm';
+import {observer} from 'mobx-react-lite'
+import { IUser } from './models/response/IUser';
+import UserService from './services/UserService';
 
-function App() {
+
+
+const App: FC = () => {
+  const {store} = useContext(Context)
+  const [users,setUsers] = useState<IUser[]>([])
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      store.checkAuth()
+    }
+
+  },[])
+  const getUsers= async ()=>{
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data)
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+  if(store.isLoading){
+    return<div>.....LOADING</div>
+  }
+  if(!store.isAuth){
+    return<LoginForm/>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{store.isAuth ? `User is authorized ${store.user.email}`:"You don't authorized!"}</h1>
+      <h1>{store.user.isActivated? `Account Activated`:"Account is not Activated"}</h1>
+      <button onClick={()=>store.logout()}>Logout</button>
+      <div><button onClick={()=>getUsers()}>Get Users</button></div>
+      {users.map(user=>
+        <div key={user.id}>{user.email}</div>)}
+    
     </div>
   );
 }
 
-export default App;
+export default observer(App);
